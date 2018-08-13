2dbdb0ef22d55566c6966f4011995ec842fac46d
s-pk <seanpkilgarriff@gmail.com>
2018-08-13 16:44:10 -0500

    add doRequest function

--- nomics/nomics.go
+++ nomics/nomics.go
@@ -1,5 +1,11 @@
 package nomics
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
 //Notes - Need a function that shows all the markets that are served. I think this might exist, but something that we can iterate through. Double check on this before committing.
 
 var (
@@ -22,4 +28,30 @@ func New(key string) ApiConfig {
 
 	return ac
 
+}
+
+func doRequest(requestURL string, object interface{}) error {
+	req, err := http.NewRequest(http.MethodGet, requestURL, nil)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		return err
+	}
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(object)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return err
 }
